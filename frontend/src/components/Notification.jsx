c68d29ff5021b8793af14c40d34faa8ee55f9a34
Jani Kaartinen <jkaart@gmail.com>
2025-08-12 15:22:14 +0300

    Frontend

---
+++ frontend/src/components/Notification.jsx
@@ -0,0 +1,19 @@
+import PropTypes from 'prop-types'
+
+const Notification = ({ type, message }) => {
+  if (message === null) {
+    return null
+  }
+  return (
+    <div className={['notification', type].join(' ')}>
+      {message}
+    </div>
+  )
+}
+
+Notification.propTypes = {
+  type: PropTypes.oneOf(['error', 'info']),
+  message: PropTypes.string,
+}
+
+export default Notification
